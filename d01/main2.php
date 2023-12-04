9554a825b15f50c1f81701a5a29f40d641ad8e16
Tjeerd Mäkel <tjeerd@myonlinestore.com>
2023-12-04 10:23:09 +0100

    Cleanup day 1 solution

--- d01/main2.php
+++ d01/main2.php
@@ -12,27 +12,6 @@ while ($line = trim(fgets($f), "\n\r")) {
     $replace = ['o1e', 't2o', 'th3ee', 'f4r', 'f5e', 's6', 's7n', 'e8t', 'n9e'];
     $line = preg_replace($search, $replace, $line);
 
-    // $search = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-    // do {
-    //     $score = [];
-    //     for ($i = 0; $i < count($search); $i++) {
-    //         $pos = strpos($line, $search[$i]);
-    //         if ($pos !== false) {
-    //             $score[$i] = $pos;
-    //         }
-    //     }
-
-    //     asort($score);
-
-    //     $found = count($score);
-    //     if ($found > 0) {
-    //         $key = array_key_first($score);
-    //         $line = str_replace($search[$key], $replace[$key], $line);
-    //     }
-    // } while ($found > 0);
-
-    $line_pass2 = $line;
-
     if (!$digits = preg_filter("/[a-z]/", "", $line)) {
         $digits = $line;
     };
@@ -45,7 +24,7 @@ while ($line = trim(fgets($f), "\n\r")) {
 
     $total = $total + $number;
 
-    echo "$line_org : $line_pass2 : $digits : $number : $total" . PHP_EOL;
+    echo "$line_org : $digits : $number : $total" . PHP_EOL;
 }
 
 echo PHP_EOL;
