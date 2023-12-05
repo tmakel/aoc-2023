6d4b5310633b62f0b1c804f7b5483a2dd74d26aa
Tjeerd Mäkel <tjeerd@myonlinestore.com>
2023-12-05 18:17:15 +0100

    Fix day 03, part 1, input works as well

--- d03/main.php
+++ d03/main.php
@@ -10,8 +10,8 @@ function readInput($file) {
     return $lines;
 }
 
-$input = readInput('example-input.txt');
-// $input = readInput('input.txt');
+// $input = readInput('example-input.txt');
+$input = readInput('input.txt');
 
 $symbol_positions = [];
 $number_positions = [];
@@ -30,14 +30,28 @@ foreach ($input as $line) {
         }
     }
 
-    // Then get number positions and their length
+    // Then get the numbers for each line and determine their position
     if (!$line_numbers = preg_filter("/\D/", '.', $line)) {
         $line_numbers = $line;
     }
 
     // var_dump($line_numbers);
 
-    $number_lines[] = array_flip(explode('.', $line_numbers));
+    $numbers = array_filter(explode('.', $line_numbers));
+    $start_pos = 0;
+    foreach ($numbers as $num) {
+        $len = strlen($num);
+        $pos = strpos($line_numbers, $num, $start_pos);
+
+        $number_lines[$line_count][] = [
+            'num' => $num,
+            'pos' => $pos,
+            'len' => $len,
+        ];
+
+        $start_pos = $pos + $len;
+    }
+
     $line_count++;
 }
 
@@ -54,65 +68,63 @@ $part_number_sum = 0;
 foreach ($number_lines as $line => $numbers) {
     echo $line . PHP_EOL;
 
-    foreach ($numbers as $number => $void) {
-        $number = (string) $number;
-        if ($number !== '') {
-            $length = strlen($number);
-            $pos_front = strpos($input[$line], $number);
-            $pos_rear = $pos_front + $length - 1;
+    foreach ($numbers as $number_details) {
+        $number = $number_details['num'];
+        $length = $number_details['len'];
+        $pos_front = $number_details['pos'];
+        $pos_rear = $pos_front + $length - 1;
 
-            $is_part_number = false;
+        $is_part_number = false;
 
-            // First check positions on same line
-            if (array_key_exists($line, $symbol_positions)) {
-                $current_line_symbols = $symbol_positions[$line];
+        // First check positions on same line
+        if (array_key_exists($line, $symbol_positions)) {
+            $current_line_symbols = $symbol_positions[$line];
 
-                //  oxxxo
-                $is_part_number = array_key_exists($pos_front - 1, $current_line_symbols) || array_key_exists($pos_rear + 1, $current_line_symbols);
-            }
-
-            // Check positions one line below
-            if (!$is_part_number && ($line < $max_symbol_line)) {
-                $line_below = $line + 1;
-                if (array_key_exists($line_below, $symbol_positions)) {
-                    $line_below_symbols = $symbol_positions[$line_below];
-
-                    //  xxx
-                    // o   o
-                    $is_part_number = array_key_exists($pos_front - 1, $line_below_symbols) || array_key_exists($pos_rear + 1, $line_below_symbols);
-                    //  xxx
-                    //  o o
-                    $is_part_number = $is_part_number || (array_key_exists($pos_front, $line_below_symbols) || array_key_exists($pos_rear, $line_below_symbols));
-                    //  xxx
-                    //   o
-                    $is_part_number = $is_part_number || (array_key_exists($pos_front + 1, $line_below_symbols) || array_key_exists($pos_rear - 1, $line_below_symbols));
-                }
-            }
-
-            // Check positions one line above
-            if (!$is_part_number && ($line > 0)) {
-                $line_above = $line - 1;
-                if (array_key_exists($line_above, $symbol_positions)) {
-                    $line_above_symbols = $symbol_positions[$line_above];
-
-                    // o   o
-                    //  xxx
-                    $is_part_number = array_key_exists($pos_front - 1, $line_above_symbols) || array_key_exists($pos_rear + 1, $line_above_symbols);
-                    //  o o
-                    //  xxx
-                    $is_part_number = $is_part_number || (array_key_exists($pos_front, $line_above_symbols) || array_key_exists($pos_rear, $line_above_symbols));
-                    //   o
-                    //  xxx
-                    $is_part_number = $is_part_number || (array_key_exists($pos_front + 1, $line_above_symbols) || array_key_exists($pos_rear - 1, $line_above_symbols));
-                }
-            }
-
-            if ($is_part_number) {
-                $part_number_sum = $part_number_sum + $number;
-            }
-
-            echo "--- $number : $pos_front : $pos_rear : " . (int) $is_part_number . " ($part_number_sum)". PHP_EOL;
+            //  oxxxo
+            $is_part_number = array_key_exists($pos_front - 1, $current_line_symbols) || array_key_exists($pos_rear + 1, $current_line_symbols);
         }
+
+        // Check positions one line below
+        if (!$is_part_number && ($line < $max_symbol_line)) {
+            $line_below = $line + 1;
+            if (array_key_exists($line_below, $symbol_positions)) {
+                $line_below_symbols = $symbol_positions[$line_below];
+
+                //  xxx
+                // o   o
+                $is_part_number = array_key_exists($pos_front - 1, $line_below_symbols) || array_key_exists($pos_rear + 1, $line_below_symbols);
+                //  xxx
+                //  o o
+                $is_part_number = $is_part_number || (array_key_exists($pos_front, $line_below_symbols) || array_key_exists($pos_rear, $line_below_symbols));
+                //  xxx
+                //   o
+                $is_part_number = $is_part_number || (array_key_exists($pos_front + 1, $line_below_symbols) || array_key_exists($pos_rear - 1, $line_below_symbols));
+            }
+        }
+
+        // Check positions one line above
+        if (!$is_part_number && ($line > 0)) {
+            $line_above = $line - 1;
+            if (array_key_exists($line_above, $symbol_positions)) {
+                $line_above_symbols = $symbol_positions[$line_above];
+
+                // o   o
+                //  xxx
+                $is_part_number = array_key_exists($pos_front - 1, $line_above_symbols) || array_key_exists($pos_rear + 1, $line_above_symbols);
+                //  o o
+                //  xxx
+                $is_part_number = $is_part_number || (array_key_exists($pos_front, $line_above_symbols) || array_key_exists($pos_rear, $line_above_symbols));
+                //   o
+                //  xxx
+                $is_part_number = $is_part_number || (array_key_exists($pos_front + 1, $line_above_symbols) || array_key_exists($pos_rear - 1, $line_above_symbols));
+            }
+        }
+
+        if ($is_part_number) {
+            $part_number_sum = $part_number_sum + $number;
+        }
+
+        echo "--- $number : $pos_front : $pos_rear : " . (int) $is_part_number . " ($part_number_sum)". PHP_EOL;
     }
 }
 
